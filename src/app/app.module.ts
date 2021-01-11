82561f1159cd0f8dc4800d3ce8293fe3627423ff
CRISTOFER OLLERO ORTIZ <collero@externos.bankia.com>
2021-01-11 08:14:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { CallbackComponent } from './components/callback/callback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'collero.eu.auth0.com',
+      clientId: 'W2encDbJ71y8dAThwTnFGjM9ikAeAgqY'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/callback/callback.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-callback',
+  templateUrl: './callback.component.html',
+  styles: [
+  ]
+})
+export class CallbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
